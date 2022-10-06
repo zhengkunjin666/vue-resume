a5905f712c67099cb75c022f5d5ec4756f555db3
zhengkunjin <zhengkunjin@126.com>
2022-10-06 23:05:39 +0800

    my resume

---
+++ docs/precache-manifest.696a7165df167ceaf331c5ef3e2aff83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "bee42074240df8b9c534",
+    "url": "css/app.001b11a3.css"
+  },
+  {
+    "revision": "bee42074240df8b9c534",
+    "url": "js/app.6ed7a8d4.js"
+  },
+  {
+    "revision": "f9d8ca9bbe18ee1f0d3e",
+    "url": "css/chunk-vendors.52eeca6f.css"
+  },
+  {
+    "revision": "f9d8ca9bbe18ee1f0d3e",
+    "url": "js/chunk-vendors.e4366f56.js"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "83107d78e24edefb6e3c3ca182058bf1",
+    "url": "index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+];
